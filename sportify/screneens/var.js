eb6e01196259b0ec4d87e5397dff9f0e78cbaa07
Med <hamma-01@live.fr>
2023-05-11 22:46:40 +0100

    m

--- sportify/screneens/var.js
+++ sportify/screneens/var.js
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 export default 'http://192.168.1.110:3000'
-=======
-<<<<<<< HEAD
-export default 'http://192.168.1.14:3000'
-=======
-export default 'http://192.168.104.17:3000'
->>>>>>> acdd60a49aab8d0c1a16effad927fadf7d4bc841
->>>>>>> 221c0d632f719c95318a4dee876b16685bf5c09d
+
